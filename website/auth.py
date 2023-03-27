baeba8f5c81e92c0ee01b3bbcbf39f4741562fc2
ujstor <astipan@gmail.com>
2023-03-27 23:03:30 +0200

    sig-up, login html and flash messages auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +13,25 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be longer than 3 chara.', category='error')
+        elif len(firstName) <2:
+            flash('Name must be longer than 1 chara.', category='error')
+        elif password1 != password2:
+            flash('Passwords dont match.', category='error')
+        elif len(password1) < 1:
+            flash('Passwords must be at least 7 chara.', category='error')
+        else:
+            flash('Account created')
+
+            #add user to database
+
     return render_template("sign_up.html")
