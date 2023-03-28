666fe86c78ecfcb68b12043baabc668ec1fae77a
ujstor <astipan@gmail.com>
2023-03-28 22:28:22 +0200

    create database

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
@@ -17,21 +21,24 @@ def logout():
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be longer than 3 chara.', category='error')
-        elif len(firstName) <2:
+        elif len(first_name) < 2:
             flash('Name must be longer than 1 chara.', category='error')
         elif password1 != password2:
             flash('Passwords dont match.', category='error')
         elif len(password1) < 1:
             flash('Passwords must be at least 7 chara.', category='error')
         else:
-            flash('Account created')
-
-            #add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
